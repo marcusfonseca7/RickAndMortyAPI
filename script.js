226de7cfbb42e1d9f0009e05d7f232063e4e497b
Marcus <marcus.fonseca968@gmail.com>
2026-08-19 19:51:20 -0300

    feat: fetch and show in the screen

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 inputCharacter = document.getElementById("input-character");
+inputCategory = document.getElementById("category");
+p = document.getElementById("paragraph");
+label = document.getElementById("label")
 
+async function catchCharacter() {
+  const category = inputCategory.value;
+  const id = inputCharacter.value;
+  const response = await fetch(`https://rickandmortyapi.com/api/${category}/${id}`);
+  const data = await response.json();
 
+  console.log(category, id)
+  console.log(data)
+  p.innerHTML = JSON.stringify(data.name);
+}
+
+inputCharacter.addEventListener("input", () => {
+    catchCharacter()
+})
