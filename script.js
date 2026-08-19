bf17357bac0905414c36c09f59889db7469b98ca
Marcus <marcus.fonseca968@gmail.com>
2026-08-19 20:03:09 -0300

    feat: reactive title and avatar

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 inputCharacter = document.getElementById("input-character");
 inputCategory = document.getElementById("category");
 p = document.getElementById("paragraph");
-label = document.getElementById("label")
+label = document.getElementById("label");
+avatar = document.getElementById("avatar");
 
 async function catchCharacter() {
   const category = inputCategory.value;
   const id = inputCharacter.value;
-  const response = await fetch(`https://rickandmortyapi.com/api/${category}/${id}`);
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/${category}/${id}`,
+  );
   const data = await response.json();
 
-  console.log(category, id)
-  console.log(data)
+  console.log(category, id);
+  console.log(data);
   p.innerHTML = JSON.stringify(data.name);
+  avatar.src = JSON.stringify(data.image).slice(1, -1);
 }
 
 inputCharacter.addEventListener("input", () => {
-    catchCharacter()
-})
+  catchCharacter();
+});
+
+inputCategory.addEventListener("change", () => {
+  console.log(inputCategory.value);
+  if (inputCategory.value === "character") {
+    label.innerHTML = "Digite o personagem que deseja ver as informações!";
+  } else if (inputCategory.value === "episode") {
+    label.innerHTML = "Digite o episódio que deseja ver as informações!";
+  } else {
+    label.innerHTML = "Digite a localização que deseja ver as informações!";
+  }
+});
